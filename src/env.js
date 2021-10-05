f857097aee47eab51313fe49c7c15f26ed21b9b5
Chris Langager <chris.langager@shopify.com>
2021-10-04 19:28:23 -0600

    init

--- src/env.js
+++ src/env.js
@@ -1,4 +1,4 @@
 export const ENV = {
   PORT: process.env.PORT || '3000',
-  POSTGRES_DATABASE_URL: process.env.POSTGRES_DATABASE_URL || 'postgres://user:password@localhost:5432/postgres',
+  POSTGRES_DATABASE_URL: process.env.DATABASE_URL || 'postgres://user:password@localhost:5432/postgres',
 };
